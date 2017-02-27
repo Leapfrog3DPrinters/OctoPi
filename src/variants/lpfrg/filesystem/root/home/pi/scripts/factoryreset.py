9d7bea4ff8dfcfd32520e7d602437f557d46ed86
Erik Heidstra <erikheidstra@live.nl>
2017-02-27 12:00:38 +0100

    Full/fast restore, Linear progress. GPIO in initramfs

--- src/variants/lpfrg/filesystem/root/home/pi/scripts/factoryreset.py
+++ src/variants/lpfrg/filesystem/root/home/pi/scripts/factoryreset.py
@@ -5,46 +5,83 @@ import time
 import RPi.GPIO as GPIO
 import subprocess
 import pygtk
-import gtk
+import gtk, gobject, glib
 
-FACTORYRESETCOMMAND = r"sudo sed -i 's@root=/dev/mmcblk0p2@root=/dev/mmcblk0p6@' /boot/cmdline.txt && sudo reboot"
+FULLFACTORYRESETCOMMAND = r"echo 'full' | sudo tee /boot/factoryreset.txt && sudo sed -i 's@root=/dev/mmcblk0p2@root=/dev/mmcblk0p6@' /boot/cmdline.txt && sudo reboot"
+FASTFACTORYRESETCOMMAND = r"echo 'fast' | sudo tee /boot/factoryreset.txt && sudo sed -i 's@root=/dev/mmcblk0p2@root=/dev/mmcblk0p6@' /boot/cmdline.txt && sudo reboot"
+
 BUTTON_PIN = 16
 
-BOUNCETIME = 1000 # minimal press interval in ms
-RESETPRESSTIME = 15 # duration of press before factory reset is initiated (in seconds)
+BOUNCETIME = 100 # minimal press interval in ms
+RESETPRESSTIME = 3 # duration of press before factory reset is initiated (in seconds)
 
-confirmation_open = False
-started = None
-ended = None
+RESPONSE_CANCEL=0
+RESPONSE_FAST=1
+RESPONSE_FULL=2
 
-def begin_press(e):
-	started = time()
+class PowerButtonHandler:
+	def __init__(self):
+		self.started = None
+		self.ended = None
+		self.confirmation_open = False
 
-def end_press(e):
-	ended = time()
-	if not started:
-		return
+	def press_event(self, e):
+		if GPIO.input(BUTTON_PIN):
+			 # Interrupt the gtk main loop
+			glib.idle_add(self.begin_press)
+		else:
+			# Interrupt the gtk main loop
+			glib.idle_add(self.end_press)
 
-	if ended-started >= RESETPRESSTIME:
-		confirm_factoryreset()
+	def begin_press(self):
+		self.started = time.time()
 
-def confirm_factoryreset():
-	#TODO: Maybe check if everything is set to open a confirmation dialog. If not, run the factory reset straight away
-	confirmation_open = True
-	message = gtk.MessageDialog(type=gtk.MESSAGE_QUESTION, buttons=gtk.BUTTONS_YES_NO)
-	message.set_markup("You are about to reset your printer to factory defaults. This will erase all your data. Are you sure you want to continue? This action cannot be undone.")
-	message.set_keep_above(True)
-	response = message.run()
-	confirmation_open = False
-	message.destroy()
+	def end_press(self):
+		self.ended = time.time()
+		if not self.started:
+			return
 
-	if response == gtk.RESPONSE_YES:
-		subprocess.Popen(FACTORYRESETCOMMAND, shell=True)
+		if self.ended-self.started >= RESETPRESSTIME:
+			self.confirm_factoryreset()
 
+	def confirm_factoryreset(self):
+		self.confirmation_open = True
+		message = gtk.Dialog(buttons=("Cancel", RESPONSE_CANCEL, "Fast", RESPONSE_FAST, "Full", RESPONSE_FULL))
+		message.set_default_size(400, 200)
+		message.set_title("Factory reset")
+		
+		image = gtk.Image()
+		image.set_from_stock(gtk.STOCK_DIALOG_WARNING, gtk.ICON_SIZE_DIALOG)
 
+		label = gtk.Label("You are about to restore your printer to factory defaults. This will erase all your data. This action cannot be undone. You may choose for a fast restore or a full restore.\n\nWhich restore would you like to carry out?")
+		label.set_line_wrap(True)
+
+		content = message.get_content_area()
+  		
+  		box = gtk.HBox()
+  		
+		box.pack_start(image, True, True, 10)
+		box.pack_start(label, True, True, 10)
+
+		box.show()
+		image.show()
+		label.show()
+
+		content.add(box)
+		message.set_keep_above(True)
+		response = message.run()
+		self.confirmation_open = False
+		message.destroy()
+		
+		if response == RESPONSE_FULL:
+			subprocess.Popen(FULLFACTORYRESETCOMMAND, shell=True)
+		elif response == RESPONSE_FAST:
+			subprocess.Popen(FASTFACTORYRESETCOMMAND, shell=True)
+
+pbh = PowerButtonHandler()
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BUTTON_PIN, GPIO.IN)
-GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=begin_press, bouncetime=BOUNCETIME)
-GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=end_press, bouncetime=BOUNCETIME)
+GPIO.add_event_detect(BUTTON_PIN, GPIO.BOTH, callback=pbh.press_event, bouncetime=BOUNCETIME)
 
+glib.threads_init() 
 gtk.main()
--- src/variants/lpfrg/filesystem/root/home/pi/scripts/poweroff.py
+++ src/variants/lpfrg/filesystem/root/home/pi/scripts/poweroff.py
@@ -1,36 +1,49 @@
 #!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
 
 COMMAND = "/usr/bin/sudo /sbin/shutdown -h now"
 LED_PIN = 22 
 PWR_PIN = 11 # Toggles power supply
 BUTTON_PIN = 16
 
-BOUNCETIME = 1000 # minimal press interval in ms
+BOUNCETIME = 100 # minimal press interval in ms
 RESETPRESSTIME = 15 # maximum press duration for regular power off
 
-def begin_press(e):
-	started = time()
+class PowerButtonHandler:
+	def __init__(self):
+		self.started = None
+		self.ended = None
 
-def end_press(e):
-	ended = time()
-	if not started:
-		return
+	def press_event(self, e):
+		if GPIO.input(BUTTON_PIN):
+			self.begin_press()
+		else:
+			self.end_press()
+			
+	def begin_press(self):
+		self.started = time.time()
 
-	if ended-started < RESETPRESSTIME:
-		begin_shutdown()
+	def end_press(self):
+		self.ended = time.time()
+		if not self.started:
+			return
 
-def begin_shutdown():
-	subprocess.call(COMMAND.split())
+		if self.ended-self.started < RESETPRESSTIME:
+			self.begin_shutdown()
 
-import RPi.GPIO as GPIO
-import time
+	def begin_shutdown(self):
+		subprocess.call(COMMAND.split())
+
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PWR_PIN, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(BUTTON_PIN, GPIO.IN)
 
-GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=begin_press, bouncetime=BOUNCETIME)
-GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=end_press, bouncetime=BOUNCETIME)
+
+pbh = PowerButtonHandler()
+GPIO.add_event_detect(BUTTON_PIN, GPIO.BOTH, callback=pbh.press_event, bouncetime=BOUNCETIME)
 
 
 while 1:
